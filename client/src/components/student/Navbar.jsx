d0d4e4de0243c9e4e9022ba2b591b77157392960
Nishant Mishra <mishranishant555@gmail.com>
2025-01-29 20:29:41 +0530

    Navbar created successfully. user autheicatoin is also done by using clerk plateform

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -7,7 +7,8 @@ const Navbar = () => {
 
 const isCourseListPage = location.pathname.includes('/course-list');
 
-
+const {openSignIn} = useClerk()
+const {user} = useUser()
 
 
   return (
@@ -15,10 +16,16 @@ const isCourseListPage = location.pathname.includes('/course-list');
        <img src={assets.logo} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
        <div className='hidden md:flex item-center gap-5 text-gray-500'>
         <div className='flex items-center gap-5'>
-          <button>Become Educator</button>
+          { user &&
+           <>
+            <button>Become Educator</button>
           <Link to='/my-enrollments' >My Enrollments</Link>
+          </>
+          }
         </div>
-        <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
+       {user ? <UserButton /> :
+       
+       <button onClick={()=> openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>}
        </div>
 
 
@@ -27,11 +34,18 @@ const isCourseListPage = location.pathname.includes('/course-list');
 
        <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
          
-         <div>
-         <button>Become Educator  </button>
-         <Link to='/my-enrollments' > My Enrollments</Link>
+         <div className='flex items-center gap-1 sm:gap-2 max-sm:text-xs'>
+         { user &&
+           <>
+            <button>Become Educator</button>
+          <Link to='/my-enrollments' >My Enrollments</Link>
+          </>
+          }
          </div>
-         <button><img src={assets.user_icon} alt="" /></button>
+         {
+          user ? <UserButton /> :  <button onClick={()=>openSignIn()}><img src={assets.user_icon} alt="" /></button>
+         }
+        
        </div>
     </div>
   )
--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../../components/student/Navbar'
+import { assets } from '../../assets/assets'
 
 const Home = () => {
   return (
-  <div>
-    
+  <div className='flex flex-col items-center justify-center w-full md:pt-36 pt-20px-7 
+  md:px-0 space-y-7 text-center bg-gradient-to-b from-cyan-100/70'>
+    <h1 className='md:text-home-heading-large text-home-heading-small relative 
+    font-bold text-gray-800 max-w-,xl mx-auto'>Empower your future with the courses designed to <span className='text-blue-600'>
+      fit your choice</span > <img className='md:block hidden absolute -bottom-7 right-0' src={assets.sketch} alt="sketch" /> </h1>
   </div>
   )
 }
