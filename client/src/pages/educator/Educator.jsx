02483fc142a940e6664de565e36283eab3ff124e
Nishant Mishra <mishranishant555@gmail.com>
2025-01-29 19:52:06 +0530

    created navbar for student section and implement the authentication using clerk package.

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -5,7 +5,10 @@ const Educator = () => {
   return (
     <div>
       <h1>Educator</h1>
+      <div>
       <Outlet />
+      </div>
+     
     </div>
   )
 }
--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from '../../components/student/Navbar'
 
 const Home = () => {
   return (
-    <div>Home page</div>
+  <div>
+    
+  </div>
   )
 }
 
