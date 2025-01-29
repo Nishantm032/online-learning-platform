1c9cfbf6f36b924e1f10dd8f37914324b9003ac6
Nishant Mishra <mishranishant555@gmail.com>
2025-01-30 05:29:06 +0530

    courseSection has created successfully. next i will add courseCard ui

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Hero from '../../components/student/Hero'
 import Companies from '../../components/student/Companies'
+import CoursesSection from '../../components/student/CoursesSection'
+
 
 
 const Home = () => {
@@ -8,6 +10,7 @@ const Home = () => {
     <div className='flex flex-col items-center space-y-7 text-center'>
 <Hero />
 <Companies />
+<CoursesSection />
     </div>
 
   )
