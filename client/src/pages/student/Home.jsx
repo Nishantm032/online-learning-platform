5ea5744517045921b7d6943d351b3078c966a272
Nishant Mishra <mishranishant555@gmail.com>
2025-02-02 12:07:35 +0530

    call to action section is created. successfully now i will create a footer for student section

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../../components/student/Hero'
 import Companies from '../../components/student/Companies'
 import CoursesSection from '../../components/student/CoursesSection'
 import TestimonialSection from '../../components/student/TestimonialSection'
+import CallToAction from '../../components/student/CallToAction'
 
 
 
@@ -13,6 +14,7 @@ const Home = () => {
 <Companies />
 <CoursesSection />
 <TestimonialSection />
+<CallToAction />
     </div>
 
   )
